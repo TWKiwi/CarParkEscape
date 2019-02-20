58554a80303bdfe262714d127ca33746490f14cb
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-02-20 23:26:31 +0800

    EscapeFromGroundFloor Failed

---
+++ CarParkEscape/EscapeTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CarParkEscape
+{
+    [TestClass]
+    public class EscapeTest
+    {
+        [TestMethod]
+        public void EscapeFromGroundFloor()
+        {
+            var result = new Kata().Escape(
+                new[,] { { 2, 0, 0, 0, 0 } });
+            var excepted = new[] { "R4" };
+            Assert.AreEqual(result, excepted);
+        }
+    }
+}
---
+++ CarParkEscape/Kata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CarParkEscape
+{
+    public class Kata
+    {
+        public string[] Escape(int[,] carPark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
