22646f8853a2d63404ddb507232df1bb4c9cd019
maxwell.chen <maxwell.chen@titansoft.com.sg>
2019-02-21 21:14:29 +0800

    EscapeFromGroundFloor_Ver2 pass

--- CarParkEscape/EscapeTest.cs
+++ CarParkEscape/EscapeTest.cs
@@ -7,11 +7,20 @@ namespace CarParkEscape
     public class EscapeTest
     {
         [TestMethod]
-        public void EscapeFromGroundFloor()
+        public void EscapeFromGroundFloor_Ver1()
         {
             var result = new Kata().Escape(
                 new[,] { { 2, 0, 0, 0, 0 } });
             var excepted = new[] { "R4" }.ToExpectedObject();
+            excepted.ShouldEqual(result);
+        }
+
+        [TestMethod]
+        public void EscapeFromGroundFloor_Ver2()
+        {
+            var result = new Kata().Escape(
+                new[,] { { 0, 2, 0, 0, 0 } });
+            var excepted = new[] { "R3" }.ToExpectedObject();
             excepted.ShouldEqual(result);
         }
     }
